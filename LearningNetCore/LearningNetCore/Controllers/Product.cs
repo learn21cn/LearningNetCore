90b450956597f5cf364d4008f5c01a2f73734c43
learn21cn <learn21cn@yeah.net>
2017-12-14 18:44:51 +0800

    第一部分更新

--- LearningNetCore/LearningNetCore/Controllers/Product.cs
+++ LearningNetCore/LearningNetCore/Controllers/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,8 +11,13 @@ namespace N1.TheStart.Controllers
     /// </summary>
     public class Product
     {
-        public int ID { get; set; }      
-        public string Name { get; set; }        
+        [Display(Name = "产品编号")]
+        public int ID { get; set; }
+        [Display(Name="产品名称")]
+        [Required(ErrorMessage ="{0}是必填项")]
+        [StringLength(15,MinimumLength =2,ErrorMessage ="{0}的最大长度不能超过{1}，最小长度不能小于{2}")]
+        public string Name { get; set; }
+        [Display(Name = "价格")]
         public double Price { get; set; }
         public ICollection<Storehouse> Storehouses { get; set; }
     }
--- LearningNetCore/LearningNetCore/Controllers/ProductsController.cs
+++ LearningNetCore/LearningNetCore/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace N1.TheStart.Controllers
@@ -49,12 +50,33 @@ namespace N1.TheStart.Controllers
             return Ok(product);
         }
 
+        //[FromBody],请求的body里面包含着方法需要的实体数据, 方法需要把这个数据Deserialize成Product.
+        //这个特性的作用在于此
         [HttpPost("create")]
         public IActionResult CreateProduct([FromBody] Product product)
         {
             if (product == null)
             {
                 return BadRequest();
+            }
+
+            if (product.Price==1)
+            {
+                //复杂的验证，可以错误信息添加到ModelState里面
+                //实际上是一个键值对
+                ModelState.AddModelError("PriceError", "价格不能为1");
+                //但是可以重复添加，键不变，值累加
+                ModelState.AddModelError("Price", "价格不能为1");
+                ModelState.AddModelError("Price", "价格不能为1");
+
+                //更复杂的验证，推荐使用FluentValidation
+                //https://github.com/JeremySkinner/FluentValidation
+            }
+
+            //验证
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
             }
 
             var maxid = InitialData.Products.Max(x => x.ID);
@@ -68,13 +90,120 @@ namespace N1.TheStart.Controllers
 
             InitialData.Products.Add(newproduct);
 
+            /*
+              对于POST，建议的返回Status Code是201(Created),可以使用CreatedAtRoute这个内置的方法. 
+              它可以返回一个带有地址Header的Response, 这个Location Header将会包含一个URI, 通过这个URI可以找到我们新创建的实体数据. 
+              这里就是指之前写的GetProduct(int id)这个方法. 但是这个Action必须有一个路由的名字才可以引用它, 
+              所以在GetProduct方法上的Route这个attribute里面加上Name="GetProduct", 然后在CreatedAtRoute方法第一个参数使用这个名字,
+              CreatedAtRoute第二个参数就是对应着GetProduct的参数列表, 使用匿名类即可,
+              最后一个参数是刚刚创建的数据实体. 
+             */
             return CreatedAtRoute("GetProduct",new { id=newproduct.ID},newproduct);
+        }
+                
+        [HttpPut("{id}")]
+        public IActionResult PutProduct(int id, [FromBody] Product product)
+        {
+            if (product == null)
+            {
+                return BadRequest();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var record = InitialData.Products.FirstOrDefault(x => x.ID == id);
+            if (record == null)
+            {
+                return NotFound();
+            }
+            //注意这里不能直接将product赋值给record，而应每个属性逐一赋值
+            record.Name=product.Name;
+            record.Price = product.Price;
+            record.Storehouses = product.Storehouses;
+
+            
+            return NoContent();
+
+        }
+
+        //可用于部分更新
+        /*注意传递数据时的格式遵循以下形式，这一点与put是不同的
+         * op 标识操作, replace 标识替换; path标识属性名, value为值.
+         * 属性名默认用小写开头即可
+        [
+            {
+                "op": "replace",
+                "path": "/name",
+                "value": "flower"
+            },
+	        {
+			    "op": "replace",
+                "path": "/price",
+                "value": "1.5"		
+	        }
+         ]
+         */
+        [HttpPatch("{id}")]
+        public IActionResult PatchProduct(int id,[FromBody] JsonPatchDocument<Product> patchDoc)
+        {
+            if (patchDoc==null)
+            {
+                return BadRequest();
+            }
+
+            var record = InitialData.Products.Find(x=>x.ID==id);
+            if (record==null)
+            {
+                return NotFound();
+            }
+
+            var toPatch = new Product
+            {
+                Name = record.Name,
+                Price=record.Price,
+                Storehouses=record.Storehouses
+
+            };
+            
+            //这句很关键
+            patchDoc.ApplyTo(toPatch,ModelState);
+
+            //添加验证，针对toPatch
+            if (toPatch.Price <1)
+            {
+                ModelState.AddModelError("Price", "产品的价格不能小于1");
+            }
+
+            //验证，注意上面“产品的价格不能小于1”的验证不需要TryValidateModel就能起作用
+            //但是，没有TryValidateModel这句话，Product类中的验证不起作用
+            TryValidateModel(toPatch);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            record.Name = toPatch.Name;            
+            record.Price = toPatch.Price;
+            record.Storehouses = toPatch.Storehouses;
+            
+            return NoContent();
         }
 
 
-
-
-
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            var model = InitialData.Products.SingleOrDefault(x => x.ID == id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            InitialData.Products.Remove(model);
+            return NoContent();
+        }
 
     }
 }
