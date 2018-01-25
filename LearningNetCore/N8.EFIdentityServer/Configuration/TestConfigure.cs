e65eb2302930a701432428bd14268af8c35a0abb
learn21cn <learn21cn@yeah.net>
2018-01-25 14:13:13 +0800

    update

---
+++ LearningNetCore/N8.EFIdentityServer/Configuration/TestConfigure.cs
@@ -0,0 +1,125 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace N8.EFIdentityServer.Configuration
+{
+    public class TestConfigure
+    {
+        public static IEnumerable<ApiResource> ApiResources()
+        {
+            return new[]
+            {
+                new ApiResource("testapi", "测试应用接口")
+                {
+                    UserClaims = new [] { "email" }
+                }
+            };
+        }
+
+        //为了使用这些openid connect scopes, 需要设置这些identity resoruces
+        public static IEnumerable<IdentityResource> IdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email()
+            };
+        }
+
+        public static IEnumerable<Client> Clients()
+        {
+            return new[]
+            {
+                new Client
+                {
+                    ClientId = "testapi",
+                    ClientSecrets = new [] { new Secret("secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                    AllowedScopes = new [] { "testapi" }
+                },
+                new Client
+                {
+                    //要与客户端指定的名称一致
+                    ClientId="mvc_implicit",
+                    ClientName="MVC Client",
+                    //使用Implicit flow时, 首先会重定向到Authorization Server, 然后登陆, 
+                    //然后Identity Server需要知道是否可以重定向回到网站, 
+                    //如果不指定重定向返回的地址的话, Session有可能就会被劫持
+                    AllowedGrantTypes=GrantTypes.Implicit,
+                    //登陆成功之后重定向的网址, 这个网址在MvcClient里, 
+                    //openid connect中间件使用这个地址就会知道如何处理从authorization server返回的response. 
+                    //这个地址将会在openid connect中间件设置合适的cookies, 以确保配置的正确性.
+                    RedirectUris ={  "http://localhost:5002/signin-oidc" },
+                    //登出之后重定向的网址. 
+                    //有可能发生的情况是, 你登出网站的时候, 会重定向到Authorization Server, 并允许从Authorization Server也进行登出动作.
+                    PostLogoutRedirectUris ={ "http://localhost:5002/signout-callback-oidc"},
+                    
+                    AllowedScopes =new List<string>
+                    {
+                        // 这里有Api resources, 还有openId connect scopes(用来限定client可以访问哪些信息)
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "testapi"
+                    },
+                    AllowAccessTokensViaBrowser = true
+                },
+                new Client
+                {
+                    //要与客户端指定的名称一致
+                    ClientId="mvc_code",
+                    ClientName="New MVC Client",                  
+                    AllowedGrantTypes=GrantTypes.HybridAndClientCredentials,
+
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    //登陆成功之后重定向的网址, 这个网址在MvcClient里, 
+                    //openid connect中间件使用这个地址就会知道如何处理从authorization server返回的response. 
+                    //这个地址将会在openid connect中间件设置合适的cookies, 以确保配置的正确性.
+                    RedirectUris ={  "http://localhost:5003/signin-oidc" },
+                    //登出之后重定向的网址. 
+                    //有可能发生的情况是, 你登出网站的时候, 会重定向到Authorization Server, 并允许从Authorization Server也进行登出动作.
+                    PostLogoutRedirectUris ={ "http://localhost:5003/signout-callback-oidc"},
+
+                    AllowedScopes =new List<string>
+                    {
+                        // 这里有Api resources, 还有openId connect scopes(用来限定client可以访问哪些信息)
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "testapi"
+                    },
+                    //我们还需要获取Refresh Token, 网站可以使用token来和api进行交互, 而不需要用户登陆到网站上                    
+                    AllowOfflineAccess = true,
+                    AllowAccessTokensViaBrowser = true
+
+                }
+            };
+        }
+
+        public static IEnumerable<TestUser> Users()
+        {
+            return new[]
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "test@yeah.net",
+                    Password = "password",
+                    Claims = new [] { new Claim("email", "test@yeah.net") }
+                }
+            };
+        }
+
+
+    }
+}
---
+++ LearningNetCore/N8.EFIdentityServer/Startup.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using N8.EFIdentityServer.Configuration;
+
+namespace N8.EFIdentityServer
+{
+    public class Startup
+    {
+        public static IConfiguration Configuration { get; private set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //配置
+
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            services.AddIdentityServer()
+                //.AddDeveloperSigningCredential()
+
+                //password为生成pfx时输入的密码
+                .AddSigningCredential(new X509Certificate2(@".\Certs\testapi.pfx", "password"))
+                //需要配置Authorization Server来允许使用这些Identity Resources
+                           
+                .AddTestUsers(TestConfigure.Users().ToList())
+                 .AddConfigurationStore(options =>
+                 {
+                     options.ConfigureDbContext = builder =>
+                         builder.UseSqlServer(connectionString,
+                             sql => sql.MigrationsAssembly(migrationsAssembly));
+                 })
+                // this adds the operational data from DB (codes, tokens, consents)
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = builder =>
+                        builder.UseSqlServer(connectionString,
+                            sql => sql.MigrationsAssembly(migrationsAssembly));
+
+                    // this enables automatic token cleanup. this is optional.
+                    options.EnableTokenCleanup = true;
+                    options.TokenCleanupInterval = 30;
+                });
+
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            InitializeDatabase(app);
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+               
+            }
+            //先用nuget安装IdentityServer
+            app.UseIdentityServer();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+        }
+        //自动迁移
+        //分别对两个context进行迁移, 然后判断是否这些表里是空的, 如果没有数据, 就把配置的内存数据添加到数据库里面
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+
+                context.Database.Migrate();
+
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in TestConfigure.Clients())
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in TestConfigure.IdentityResources())
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in TestConfigure.ApiResources())
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
